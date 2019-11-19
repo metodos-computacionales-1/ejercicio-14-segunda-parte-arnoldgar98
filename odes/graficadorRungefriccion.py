645c00109e452060de2c436b23d9cca92e9558e9
Default user <as.garcia11@uniandes.edu.co>
2019-11-19 17:20:53 +0000

    terminado ahora si

---
+++ odes/graficadorRungefriccion.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+data = np.loadtxt('datafriccion.dat')
+tiempo=data[:,0]
+x=data[:,1]
+v=data[:,2]
+
+plt.figure()
+plt.plot(tiempo,x,label='posición')
+plt.xlabel('t(s)')
+plt.ylabel('x(m)')
+plt.title("Posición para armónico simple")
+plt.grid()
+plt.legend()
+plt.savefig('posicionRungefriccion.png')
+
+plt.figure()
+plt.plot(x,v,label="X vs V")
+plt.xlabel('x(m)')
+plt.ylabel('v(m/s)')
+plt.title("velocidad para armonico simple")
+plt.grid()
+plt.legend()
+plt.savefig('velocidadRungefriccion.png')
